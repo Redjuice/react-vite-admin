006f97758a12d81329530727b794d1eb6a0590b6
wangheng <wangheng@vchangyi.com>
2021-11-30 16:46:01 +0800

    del: ❌ 增加待删除文件

--- src/Layout/MyContent/index.jsx
+++ src/Layout/MyContent/index.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import List from '@/views/myapp/module1/list/list';
-// import Form from '@/views/myapp/module1/form/form';
 
 import './index.less';
 export default class MyContent extends Component {
   render() {
     return (
       <div className="content" data-component="content">
-        <List />
-        {/* content */}
+        content
       </div>
     );
   }
